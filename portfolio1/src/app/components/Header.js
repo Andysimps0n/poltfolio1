d5c2f109ba6e86ccf5bb92972d64b16d6bf898d0
Andy Kim <26eckim@wonhago.org>
2025-01-12 21:53:43 +0900

    changed structure and added menu UI

--- portfolio1/src/app/components/Header.js
+++ portfolio1/src/app/components/Header.js
@@ -1,12 +1,43 @@
-import React from 'react'
+"use client"
+
+import React, {useState} from 'react'
 
 function Header() {
+
+  const [isOnProject, setIsOnProject] = useState(false)
+  
   return (
+    <>
+    
     <div className='header-container '>
       <div className="header-wrapper ">
-        <div className='header-element '>Sam Kim</div>
+        <div className='header-element'>Sam Kim</div>
+        <div onClick={()=>{setIsOnProject(!isOnProject); console.log('clicked')}} className='header-element relative'>
+          {isOnProject ? "Close" : "Project"}
+        </div>
+      </div>
+
+    </div>
+
+
+    {isOnProject ? 
+    <div className="project-menu-wrapper ">
+      <div className="project-menu-header">
+        <div className="header-wrapper ">
+          <div className='header-element' style={{opacity : 0}}>Sam Kim</div>
+          <div onClick={()=>{setIsOnProject(!isOnProject)}} className='header-element hover paddingBox1'>
+            {isOnProject ? "Close" : "Project"}
+          </div>
+        </div>
+      </div>
+
+      <div className="project-menu">
+        <div className="project-text">project1</div>
+        <div className="project-text">project1</div>
       </div>
     </div>
+    : null}
+    </>
   )
 }
 
